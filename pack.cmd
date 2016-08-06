3f2f1c7dc96983fbd3bae9f883f27bdf57c62484
ryrun <ryrun@online.de>
2016-08-06 22:28:43 +0200

    move final file to out/

--- pack.cmd
+++ pack.cmd
@@ -1,2 +1,3 @@
 @echo off
-"C:\Program Files\7-Zip\7z" a -tzip toimp.ryrun.MidifighterButtons.xrnx main.lua manifest.xml 
+del /F /Q out/toimp.ryrun.MidifighterButtons.xrnx
+"C:\Program Files\7-Zip\7z" a -tzip out/toimp.ryrun.MidifighterButtons.xrnx main.lua manifest.xml 
